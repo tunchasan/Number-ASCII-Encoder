613ac409dcccc395807b664856ceb139cd078232
tunchasan <hasantunc_ogr@outlook.com>
2020-08-16 04:19:26 +0300

    Fill ListManager' addFirstElemToList function body

--- ConsoleApplication1/ConsoleApplication1/ListManager.cpp
+++ ConsoleApplication1/ConsoleApplication1/ListManager.cpp
@@ -2,17 +2,24 @@
 
 void ListManager::addFirstElemToList(int value)
 {
+	// Create new node
+	Node* newNode = new Node(value);
 
+	root = newNode;
+
+	newNode->prev = root;
+
+	newNode->next = root;
 }
 
 void ListManager::addElemToLeft(int value, int iterationCount, Node* from)
 {
-
+	// TODO
 }
 
 void ListManager::addElemToRight(int value, Node* from)
 {
-
+	// TODO
 }
 
 Node * ListManager::getLinkedList()
