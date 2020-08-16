e84a00fc9aa1db1608833c2e92231020267449c6
tunchasan <hasantunc_ogr@outlook.com>
2020-08-16 04:03:07 +0300

    Create ListManager' functions' forward declaretion

--- ConsoleApplication1/ConsoleApplication1/ListManager.cpp
+++ ConsoleApplication1/ConsoleApplication1/ListManager.cpp
@@ -1,1 +1,26 @@
 #include "ListManager.h"
+
+void ListManager::addFirstElemToList(int value)
+{
+
+}
+
+void ListManager::addElemToLeft(int value, int iterationCount, Node* from)
+{
+
+}
+
+void ListManager::addElemToRight(int value, Node* from)
+{
+
+}
+
+Node * ListManager::getLinkedList()
+{
+	return root;
+}
+
+ListManager::ListManager()
+{
+	root = nullptr;
+}
--- ConsoleApplication1/ConsoleApplication1/ListManager.h
+++ ConsoleApplication1/ConsoleApplication1/ListManager.h
@@ -4,10 +4,32 @@
 #include <string>
 #include <iostream>
 
+#include "Node.h"
+
 using namespace std;
 
 struct ListManager {
 
+private:
+
+	Node* root;
+
+public:
+
+	// Adds new elem. to Linked List
+	void addFirstElemToList(int);
+
+	// Adds new elem. to left side of Linked List
+	void addElemToLeft(int, int, Node*);
+
+	// Adds new elem. to right side of Linked List
+	void addElemToRight(int, Node*);
+
+	// Returns LinkedList' root
+	Node* getLinkedList();
+
+	// Constructor
+	ListManager();
 };
 
 #endif
